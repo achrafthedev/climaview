64b633562bbdb56589c7abb85decfe03aa9d37ac
Achraf <92728819+achrafthedev@users.noreply.github.com>
2025-03-08 01:00:50 +0100

    Update MapComponent.js

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,3 +1,5 @@
+// Not used for now!
+
 import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
